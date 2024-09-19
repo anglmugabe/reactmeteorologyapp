4908b132e2309dbd5b6517cad231312e625355ac
anglmugabe <anglmugabe@gmail.com>
2024-09-19 13:24:35 +0200

    added all files from old weatherapp

--- src/App.js
+++ src/App.js
@@ -5,34 +5,39 @@ import "./App.css";
 export default function App() {
 	return (
 		<div className="App">
-			<Weather />
-			<footer>
-				This project was coded by{" "}
-				<a
-					href="https://www.shecodes.io/graduates/105285-angela-mugabe"
-					target="_blank"
-				>
-					{" "}
-					Angela Mugabe{" "}
-				</a>{" "}
-				, is open sourced on{" "}
-				<a
-					href="https://github.com/anglmugabe/weather-react-application"
-					target="_blank"
-				>
-					{" "}
-					GitHub
-				</a>
-				and hosted on
-				<a
-					href="https://appmeteorologicoreact.netlify.app/"
-					target="_blank"
-				>
-					{" "}
-					Netlify{" "}
-				</a>
-				.
-			</footer>
+			<div className="container">
+				<Weather defaultCity="Maputo" />
+				<footer>
+					This project was coded by{" "}
+					<a
+						href="https://www.shecodes.io/graduates/105285-angela-mugabe"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{" "}
+						Angela Mugabe{" "}
+					</a>{" "}
+					, is open sourced on{" "}
+					<a
+						href="https://github.com/anglmugabe/weather-react-application"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{" "}
+						GitHub
+					</a>
+					and hosted on
+					<a
+						href="https://appmeteorologicoreact.netlify.app/"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						{" "}
+						Netlify{" "}
+					</a>
+					.
+				</footer>
+			</div>
 		</div>
 	);
 }
